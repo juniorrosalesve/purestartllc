1d83822e164d2105f919f0b5bda1060098b6e89f
Júnior Rosales <barcomunidad08@gmail.com>
2022-02-03 10:39:35 -0400

    cambios

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>PureStartLLC</title>
+    <title>PURESTART, LLC</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -29,31 +29,26 @@
         <nav class="bg-white border-b-2 border-primary px-2 sm:px-4 py-2.5">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="#" class="flex">
-                    <img src="{{ asset('img/logo2.png') }}" class="w-64">
+                    <img src="{{ asset('img/xlogo.png') }}" class="w-64">
                 </a>
                 <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
                     <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium" id="main-menu">
                         <li>
-                            <a href="javscript:void(0)" class="active menu-item block py-2 pr-4 pl-3 text-primary-focus border-b border-gray-100 md:border-none md:bg-transparent md:p-0" aria-current="page">Home</a>
+                            <a href="javscript:void(0)" class="active menu-item uppercase block py-2 pr-4 pl-3 text-primary-focus border-b border-gray-100 md:border-none md:bg-transparent md:p-0" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="javscript:void(0)" class="menu-item block py-2 pr-4 pl-3 text-primary-focus border-b border-gray-100 md:border-0 md:hover:text-secondary md:p-0">
-                                Our services
+                            <a href="javscript:void(0)" class="menu-item uppercase block py-2 pr-4 pl-3 text-primary-focus border-b border-gray-100 md:border-0 md:hover:text-secondary md:p-0">
+                                About
                             </a>
                         </li>
                         <li>
-                            <a href="javscript:void(0)" class="menu-item block py-2 pr-4 pl-3 text-primary-focus border-b border-gray-100 md:border-0 md:hover:text-secondary md:p-0">
+                            <a href="javscript:void(0)" class="menu-item uppercase block py-2 pr-4 pl-3 text-primary-focus border-b border-gray-100 md:border-0 md:hover:text-secondary md:p-0">
+                                Services
+                            </a>
+                        </li>
+                        <li>
+                            <a href="javscript:void(0)" class="menu-item uppercase block py-2 pr-4 pl-3 text-primary-focus md:border-0 md:hover:text-secondary md:p-0">
                                 Contact
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javscript:void(0)" class="menu-item block py-2 pr-4 pl-3 text-primary-focus md:border-0 md:hover:text-secondary md:p-0">
-                                Option
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javscript:void(0)" class="menu-item block py-2 pr-4 pl-3 text-primary-focus md:border-0 md:hover:text-secondary md:p-0">
-                                Option
                             </a>
                         </li>
                     </ul>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -4,13 +4,13 @@
         <div class="swiper mySwiper md:max-h-96 shadow-lg">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <img src="https://via.placeholder.com/1600x650">
+                    <img src="{{ asset('img/sliders/01x650.png') }}">
                 </div>
                 <div class="swiper-slide">
-                    <img src="https://via.placeholder.com/1600x650">
+                    <img src="{{ asset('img/sliders/02x650.png') }}">
                 </div>
                 <div class="swiper-slide">
-                    <img src="https://via.placeholder.com/1600x650">
+                    <img src="https://via.placeholder.com/1500x450">
                 </div>
             </div>
             <div class="swiper-button-next"></div>
@@ -20,25 +20,19 @@
     </section>
     
     <section id="services">
-        <div class="grid grid-cols-1 md:grid-cols-2">
+        <div class="w-full text-center p-10">
+            <h1 class="mb-3 mt-1 md:mt-5 lato uppercase text-2xl">About Pure</h1>
+            <p class="lato uppercase">PureStart is a local owner-operated commercial cleaning company that offers a range
+                of cleaning services, performing with exceptional cleaning knowledge and high-quality
+                products to guarantee customer satisfaction and exceed expectations. Serving the
+                Denver metro area.</p>
+            <p class="lato uppercase mb-3 md:mt-3">WE WILL MAKE SURE YOUR BUSINESS IS CLEAN, READY FOR YOU AND
+                YOUR EMPLOYEES TO HAVE A GOOD, FRESH, PURE START.  <br />
+                WE ARE RELIABLE, TRUSTWORTHY AND DETAIL ORIENTED.</p>
+        </div>
+        {{-- <div class="grid grid-cols-1 md:grid-cols-2">
             <div class="basis-2/4 p-10 bg-secondary-focus">
-                <h1 class="mb-3 mt-1 md:mt-5 lato uppercase text-2xl">Our services</h1>
-                <p class="lato">Vacant Units
-                    Before & After Party
-                    Events Cleaning
-                    Corporate Offices
-                    Restaurants
-                    Showrooms
-                    Move In/Out Cleaning
-                    Vacation Rentals
-                    Leasing Offices
-                    Medical Offices
-                    Classrooms/ Schools
-                    Real State Listings
-                    New Construction Units
-                    Retail Stores
-                    Storage Units
-                    Club Houses</p>
+
             </div>
             <div class="basis-2/4 p-10 bg-primary">
                 <h1 class="text-center text-white text-lg mb-5 lato">We will make sure your business is clean,
@@ -55,56 +49,137 @@
                     
                     satisfaction and exceed your expectations.</p>
             </div>
-        </div>
+        </div> --}}
         <div class="w-full bg-white p-7">
-            <h1 class="text-center uppercase lato">BOOK NOW!! 20% Discount On Our First Visit</h1>
+            <h1 class="text-center uppercase lato">BOOK NOW!! <br /> 20% Discount On Our First Visit</h1>
         </div>
     </section>
     <section>
         <div class="grid grid-cols-1 md:grid-cols-2">
-            <div id="map" class="w-full h-52 md:h-full"></div>
+            <div class="w-full py-10 px-5 bg-primary">
+                <h1 class="text-center text-white text-xl mb-1 lato uppercase">
+                    • HAVING A CLEAN SPACE IS SIMPLE •
+                </h1>
+                {{-- <h1 class="text-center text-white text-md lato uppercase">We will make sure your business is clean,
+                    ready for you and your employees to
+                    have a good-fresh, Pure-Start.</h1> --}}
+                <div class="flex mt-10">
+                    <div class="w-1/4">
+                        <div class="relative mb-2">
+                            <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                            <span class="text-center text-secondary-focus w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                </svg>
+                            </span>
+                            </div>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">Contact us</div>
+                    </div>
+                    <div class="w-1/4">
+                        <div class="relative mb-2">
+                            <div class="absolute flex align-center items-center align-middle content-center" style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
+                            <div class="w-full bg-gray-200 rounded items-center align-middle align-center flex-1">
+                                <div class="w-0 py-1 rounded" style="width: 33%;"></div>
+                            </div>
+                            </div>
+                            <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                                <span class="text-center text-secondary-focus w-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">
+                            SET UP AN APPOINTMENT <br /> <span class="text-xs">(Free estimate)</span>
+                        </div>
+                    </div>
+                    <div class="w-1/4">
+                        <div class="relative mb-2">
+                            <div class="absolute flex align-center items-center align-middle content-center" style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
+                            <div class="w-full bg-gray-200 rounded items-center align-middle align-center flex-1">
+                                <div class="w-0 py-1 rounded" style="width: 33%;"></div>
+                            </div>
+                            </div>
+                            <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                            <span class="text-center text-secondary-focus w-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">Book</div>
+                    </div>
+                    <div class="w-1/4">
+                        <div class="relative mb-2">
+                            <div class="absolute flex align-center items-center align-middle content-center" style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
+                            <div class="w-full bg-gray-200 rounded items-center align-middle align-center flex-1">
+                                <div class="w-0 bg-green-300 py-1 rounded" style="width: 0%;"></div>
+                            </div>
+                            </div>
+                            <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                                <span class="text-center text-secondary-focus w-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">Enjoy your clean space</div>
+                    </div>
+                </div>
+            </div>
             <div class="p-10 bg-white md:bg-secondary-focus">
-                <h1 class="text-2xl text-center lato uppercase">Contact</h1>
-                <p class="text-sm text-center lato">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod dicta nostrum est culpa aut esse sunt!</p>
+                <p class="text-md text-center lato uppercase mb-2">Drop us a line and we’ll get back with you soon.</p>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
                     <div class="form-control">
                         <label class="label">
-                            <span class="label-text lato">Full name</span>
+                            <span class="label-text lato uppercase">Full name</span>
                         </label> 
-                        <input type="text" class="input input-bordered input-sm">
+                        <input type="text" class="rounded-sm text-xs" placeholder="Jhon Doe">
                     </div> 
                     <div class="form-control">
                         <label class="label">
-                            <span class="label-text lato">E-mail</span>
+                            <span class="label-text lato uppercase">Email address</span>
                         </label> 
-                        <input type="text" class="input input-bordered input-sm">
-                    </div> 
+                        <input type="text" class="rounded-sm text-xs" placeholder="jhon.doe@company.com">
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text lato uppercase">Phone number</span>
+                        </label> 
+                        <input type="text" class="rounded-sm text-xs" placeholder="(000) 111-1111">
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text lato uppercase">Business name</span>
+                        </label> 
+                        <input type="text" class="rounded-sm text-xs" placeholder="Company name">
+                    </div>
                 </div>
-                <div class="grid grid-cols-1 gap-3">
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text lato">Message</span>
-                        </label> 
-                        <textarea class="textarea h-10 textarea-bordered"></textarea>
-                    </div>
-                    <div>
-                        <center>
-                            <button type="button" class="btn btn-success btn-sm lato">Submit</button>
-                        </center>
-                    </div>
+                <div>
+                    <center>
+                        <button class="btn btn-success btn-sm w-60 lato border-0 rounded-sm mt-4">Submit</button>
+                    </center>
                 </div>
             </div>
         </div>
     </section>
     <footer>
         <div class="md:flex md:flex-row bg-white">
-            <div class="md:basis-2/6 p-10 bg-primary text-white">
+            <div class="md:basis-2/6 p-10" style="background-color: #f6f8d8;">
                 <h1>LICENSED & INSURED</h1>
                 <p>Reliable, Trustworthy & Detail Oriented</p>
                 <div class="grid grid-cols-3 mt-2">
-                    <span class="mdi mdi-instagram text-secondary-focus cursor-pointer" style="font-size: 40px;"></span>
-                    <span class="mdi mdi-facebook text-blue-300 cursor-pointer" style="font-size: 40px;"></span>
-                    <span class="mdi mdi-twitter text-blue-200 cursor-pointer" style="font-size: 40px;"></span>
+                    <a href="https://www.instagram.com/purestartllc/" target="_blank">
+                        <span class="mdi mdi-instagram cursor-pointer" style="font-size: 40px;color:#c0ccbe;"></span>
+                    </a>
+                    <span class="mdi mdi-facebook cursor-pointer" style="font-size: 40px;color:#326a58;"></span>
+                    <a href="https://twitter.com/PureStartllc" target="_blank">
+                        <span class="mdi mdi-twitter cursor-pointer" style="font-size: 40px;color:#344243;"></span>
+                    </a>
                 </div>
             </div>
             <div class="md:basis-8/12 p-10">

7269cd7fdbf48faf2812cc2339cf1337c8101b3f
Júnior Rosales <barcomunidad08@gmail.com>
2022-02-03 15:07:11 -0400

    cambios

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -61,15 +61,64 @@
                 {{-- <h1 class="text-center text-white text-md lato uppercase">We will make sure your business is clean,
                     ready for you and your employees to
                     have a good-fresh, Pure-Start.</h1> --}}
-                <div class="flex mt-10">
-                    <div class="w-1/4">
-                        <div class="relative mb-2">
-                            <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                <div class="grid justify-items-center sm:hidden">
+                    <div data-aos="fade-up" class="my-2">
+                        <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
                             <span class="text-center text-secondary-focus w-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                 </svg>
                             </span>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">Contact us</div>
+                    </div>
+                    <div class="w-2 rounded bg-gray-200 h-10" data-aos="zoom-in"></div>
+                    <div data-aos="fade-up" class="my-2">
+                        <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                            <span class="text-center text-secondary-focus w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">
+                            SET UP AN APPOINTMENT <br /> <span class="text-xs">(Free estimate)</span>
+                        </div>
+                    </div>
+                    <div class="w-2 rounded bg-gray-200 h-10" data-aos="zoom-in"></div>
+                    <div data-aos="fade-up" class="my-2">
+                        <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                            <span class="text-center text-secondary-focus w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">Book</div>
+                    </div>
+                    <div class="w-2 rounded bg-gray-200 h-10" data-aos="zoom-in"></div>
+                    <div data-aos="fade-up" class="my-2">
+                        <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                            <span class="text-center text-secondary-focus w-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="lato uppercase text-white text-sm text-center">Enjoy your clean space</div>
+                    </div>
+                </div>
+
+
+                <div class="hidden sm:flex sm:mt-10" data-aos="fade-up">
+                    <div class="w-1/4">
+                        <div class="relative mb-2">
+                            <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
+                                <span class="text-center text-secondary-focus w-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                    </svg>
+                                </span>
                             </div>
                         </div>
                         <div class="lato uppercase text-white text-sm text-center">Contact us</div>
@@ -101,7 +150,7 @@
                             </div>
                             </div>
                             <div class="w-10 h-10 mx-auto bg-primary-focus rounded-full text-lg text-white flex items-center">
-                            <span class="text-center text-secondary-focus w-full">
+                                <span class="text-center text-secondary-focus w-full">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                     </svg>

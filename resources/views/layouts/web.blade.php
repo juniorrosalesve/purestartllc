826103f422081e8192586f4026ead93dd6721888
Júnior Rosales <barcomunidad08@gmail.com>
2022-02-21 07:09:00 -0400

    Update web.blade.php

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -128,8 +128,8 @@
     </footer>
     
     <script src="{{ mix('js/purestartllc.js') }}"></script>
-    {{-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script> --}}
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    {{-- <script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script> --}}
 
     <script>
         var swiper = new Swiper(".mySwiper", {

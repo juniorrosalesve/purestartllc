6f3d336326be759a2a83f894850d8df7c272c83f
Júnior Rosales <barcomunidad08@gmail.com>
2022-02-23 08:46:37 -0400

    cambios

--- resources/views/livewire/site/services.blade.php
+++ resources/views/livewire/site/services.blade.php
@@ -1,6 +1,6 @@
 <div>
    <h1 class="text-center uppercase lato text-2xl mt-10 font-semibold">WE CLEAN</h1>
-   <div class="p-10 md:p-5 md:flex md:justify-center gap-11">
+   <div class="p-10 md:p-5 md:flex md:justify-center">
       <div class="w-full md:w-fit bg-white rounded-lg border border-gray-200 shadow-md">
          <div class="sm:flex sm:justify-center md:flex-none	md:block xl:flex p-5">
             <div>
@@ -19,7 +19,7 @@
             </div>
          </div>
       </div>
-      <div class="w-full md:w-fit bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:mt-0">
+      <div class="w-full md:w-fit bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:ml-5 md:mt-0">
          <div class="sm:flex sm:justify-center md:flex-none	md:block xl:flex p-5">
             <div >
                <img src="{{ asset('img/servicios/IMG-3065-min.png') }}" alt="notas" class="w-56 sm:w-40" />
@@ -35,7 +35,7 @@
             </div>
          </div>
       </div>
-      <div class="w-full md:w-fit bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:mt-0">
+      <div class="w-full md:w-fit bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:ml-5 md:mt-0">
          <div class="sm:flex sm:justify-center md:flex-none	md:block xl:flex p-5">
             <div >
                <img src="{{ asset('img/servicios/IMG-3064-min.png') }}" alt="notas" class="w-56 sm:w-40" />
@@ -77,7 +77,7 @@
    </div>
 
    <h1 class="text-center uppercase lato text-2xl mt-10 font-semibold">OUR SERVICES</h1>
-   <div class="p-10 md:grid md:grid-cols-2 xl:grid-cols-none xl:flex xl:justify-center gap-10">
+   <div class="p-10 md:grid md:grid-cols-2 xl:grid-cols-none xl:flex xl:justify-center">
       <div class="w-full sm:flex sm:justify-between md:flex md:block md:w-72 bg-white rounded-lg border border-gray-200 shadow-md">
          <div class="p-5">
             <div >
@@ -93,7 +93,7 @@
             </div>
          </div>
       </div>
-      <div class="w-full md:w-72 bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:mt-0">
+      <div class="w-full md:w-72 bg-white rounded-lg border border-gray-200 shadow-md mt-5 xl:ml-7 xl:mt-0">
          <div class="p-5">
             <div >
                <img src="{{ asset('img/servicios/IMG-3062-min.png') }}" alt="notas" class="w-56 sm:w-40" />
@@ -109,7 +109,7 @@
             </div>
          </div>
       </div>
-      <div class="w-full md:w-72 bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:mt-0">
+      <div class="w-full md:w-72 bg-white rounded-lg border border-gray-200 shadow-md mt-5 xl:ml-7 xl:mt-0">
          <div class="p-5">
             <div >
                <img src="{{ asset('img/servicios/IMG-3061-min.png') }}" alt="notas" class="w-56 sm:w-40" />
@@ -124,7 +124,7 @@
          </div>
       </div>
 
-      <div class="w-full md:w-72 bg-white rounded-lg border border-gray-200 shadow-md mt-5 md:mt-0">
+      <div class="w-full md:w-72 bg-white rounded-lg border border-gray-200 shadow-md mt-5 xl:ml-7 xl:mt-0">
          <div class="p-5">
             <div >
                <img src="{{ asset('img/servicios/notas-min.png') }}" alt="notas" class="w-56 sm:w-40" />
